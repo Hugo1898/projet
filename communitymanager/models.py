ac018d9a6b4a8bfc2efcb2351e32a5c6a1501369
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2021-05-18 12:38:11 +0200

    début projet

--- communitymanager/models.py
+++ communitymanager/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Communaute(models.Model):
+    nom = models.CharField(max_length=200)
+    abonnes = models.ManyToManyField(User, related_name="communautes")
+
+    class Meta:
+        verbose_name = "Communaute"
+
+    def __str__(self):
+        return self.nom
+
--- projet/urls.py
+++ projet/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import LoginView, logout_then_login
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('communitymanager/', include('communitymanager.urls')),
+    path('connexion/', LoginView.as_view(template_name='registration/login.html'), name="login"),
+    path('deconnexion/', logout_then_login, name="logout"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
