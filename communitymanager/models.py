3f91d19686923ad5dfdd252d733fe0ad484b2022
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2021-05-24 14:32:35 +0200

    mise en page débuté

--- communitymanager/models.py
+++ communitymanager/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Communaute(models.Model):
     nom = models.CharField(max_length=200)
     abonnes = models.ManyToManyField(User, related_name="communautes")
+    description = models.TextField()
 
     class Meta:
         verbose_name = "Communaute"
